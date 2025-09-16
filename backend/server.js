21d0c0d4cd1b167abe59a94b73694bbe2e7057bd
bhepautomation-ui <bhep.automation@gmail.com>
2025-09-16 22:49:59 +0700

    feat: add basic backend server

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Backend siap!'); // Untuk cek jika server hidup
+});
+
+io.on('connection', (socket) => {
+  console.log('Client connected:', socket.id);
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
